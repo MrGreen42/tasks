0b5b8658a6e0e5b450b47ac0dcd9c432f09f5c8d
MrGreen42 <ludvikjohn@mail.ru>
2020-11-21 02:19:21 +0300

    Update 6,1.c
    change using of lstat

--- 6,1.c
+++ 6,1.c
@@ -19,22 +19,33 @@ int main(int argc, char *argv[]) {
 	struct stat stat_buf;
 
 	while ((dir = readdir(dirp)) != NULL ) {
-		if (lstat (dir->d_name, &stat_buf) == -1) {
-                	perror("Failed to stat");
-                	return 1;
-		}
 		printf("File type: ");
-
-	        switch (stat_buf.st_mode & S_IFMT) {
-        		case S_IFBLK:  printf("block device		");        break;
-       	   		case S_IFCHR:  printf("character device		");        break;
-          		case S_IFDIR:  printf("directory		");        break;
-         		case S_IFIFO:  printf("FIFO/pipe		");        break;
-	        	case S_IFLNK:  printf("symlink			");        break;
-	        	case S_IFREG:  printf("regular file		");        break;
-	        	case S_IFSOCK: printf("socket			");        break;
-	        	default:       printf("unknown?			");        break;
-	        }
+		if ((dir->d_type) == DT_UNKNOWN) {
+			if (lstat (dir->d_name, &stat_buf) == -1) {
+                	        perror("Failed to stat");
+        	                return 1;
+	                }
+	        	switch (stat_buf.st_mode & S_IFMT) {
+        			case S_IFBLK:  printf("block device		");        break;
+       	   		    case S_IFCHR:  printf("character device	");        break;
+          			case S_IFDIR:  printf("directory		");        break;
+         			case S_IFIFO:  printf("FIFO/pipe		");        break;
+	        		case S_IFLNK:  printf("symlink		    ");        break;
+	        		case S_IFREG:  printf("regular file		");        break;
+	        		case S_IFSOCK: printf("socket			");        break;
+	        		default:       printf("unknown?		    ");        break;
+	        	}
+		} else {
+			switch (dir->d_type) {
+				case DT_BLK:   printf("block device		        ");	       break;
+				case DT_DIR:   printf("directory                ");        break;
+				case DT_CHR:   printf("character device         ");        break;
+				case DT_FIFO:  printf("FIFO/pipe                ");        break;
+                case DT_LNK:   printf("symlink                  ");        break;
+				case DT_REG:   printf("regular file             ");        break;
+				case DT_SOCK:  printf("socket                   ");        break;
+			}
+		}
 		printf("Name: %s \n", dir->d_name);
 
         }
